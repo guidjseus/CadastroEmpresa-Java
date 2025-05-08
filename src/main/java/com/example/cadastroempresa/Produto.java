70e470d61be9649a7a963b7897e826dafa9bd47b
GuilhermeSLJ <2004guisilva@gmail.com>
2025-05-08 10:51:14 -0300

    Feature: Classe Produto criada:

--- src/main/java/com/example/cadastroempresa/Produto.java
+++ src/main/java/com/example/cadastroempresa/Produto.java
@@ -1,4 +1,58 @@
 package com.example.cadastroempresa;
 
 public class Produto {
+
+    private String nome;
+    private String codigo;
+    private String descricao;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, String codigo, String descricao, double preco, int quantidade) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
 }
